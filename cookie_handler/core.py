1e17ea6628f314a3d5d9749d6f9ba0e830189798
eliich <142771527+eliich@users.noreply.github.com>
2023-11-30 19:08:44 -0800

    Create core.py

---
+++ cookie_handler/core.py
@@ -0,0 +1,14 @@
+# cookie_handler.py
+import json
+
+def read_and_modify_cookies(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            cookies = json.load(file)
+            for cookie in cookies:
+                if cookie['sameSite'] not in ['Strict', 'Lax', 'None']:
+                    cookie['sameSite'] = 'None'  # Adjust as needed
+            return cookies
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
