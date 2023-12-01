5590d631ad5f6cc789f73f74fc1f723388c0d500
eliich <142771527+eliich@users.noreply.github.com>
2023-11-30 21:45:40 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cookie_handler',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A Python package to handle cookies',
+    author='eliich',
+    author_email='example@example.com',  # Placeholder email
+    url='https://github.com/eliich/cookie-handler',
+    install_requires=[],  # No dependencies
+)
